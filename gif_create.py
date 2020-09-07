015b392860e6b84a425b7315f27a77f8db9102f3
kristmundur <kristmunduragust@gmail.com>
2020-09-07 14:49:32 +0000

    Regular FCM without the spatial properties"

---
+++ gif_create.py
@@ -0,0 +1,13 @@
+import imageio
+import os
+
+folder = "/home/kristmundur/Documents/KTH/Project Course/sFCM/gifs"
+ims = []
+for (s, w, files) in os.walk(folder):
+    for f in sorted(list(files), key = lambda x : int(x.split('.')[0])):
+        ims.append(os.path.join(folder, f))
+        print(f)
+
+images = [imageio.imread(f) for f in ims]
+
+imageio.mimsave(os.path.join(folder, 'out.gif'), images, duration = 1)
