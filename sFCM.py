72e221fa9e060e6815ea2e9e598c793bf75bb9b7
kristmundur <kristmunduragust@gmail.com>
2020-09-16 10:24:09 +0000

    Both FCM and sFCM now included

--- sFCM.py
+++ sFCM.py
@@ -1,42 +1,32 @@
 import numpy as np
 
-class sFCM:
+class FCM:
     MAX_ITER = 50
-    def __init__(self, m, K, p, q, w_siz):
+    def __init__(self, m, K, imshape):
         '''
         Initializes a sFCM algorithm with parameters
 
             m       - integer that controls the fuzziness
             K       - number of classes
-            p       - integer that controls importance of membership function
-            q       - integer that controls importance of spatial function
-            w_size  - odd integer controlling the window size for spatial function (w_size x w_size)
-            Note (p,q) = (1,0) => regular FCM
+            imshape - np array (h, w, c)
         '''
-        assert w_siz % 2 != 0
+        self.m       = m
+        self.K       = K
+        self.imshape = imshape
 
-        self.m = m
-        self.K = K
-        self.p = p
-        self.q = q
-        self.w_siz = w_siz
+        #initialize random memberships
+        self.u = self.__u_init(*imshape[:2])
+
+        #intialize mapping functions
+        self.dec = self.decoder(imshape[1])
+        self.enc = self.encoder(imshape[1])
+
 
     def decoder(self, w):
         return lambda j : (j // w, j % w)
 
     def encoder(self, w):
         return lambda i, j : w*i + j
-
-    def __nb(self, j, mx):
-
-        i, j = self.dec(j)
-
-        imx, jmx = self.dec(mx -1)
-        ip, jp = max(0, i - self.w_siz//2), max(0, j - self.w_siz//2)
-
-        for x in range(ip, ip + self.w_siz):
-            for xx in range(jp, jp + self.w_siz):
-                yield self.enc(min(imx, x), min(jmx, xx)) 
 
     def __u_init(self, w, h):
         '''
@@ -49,16 +39,8 @@ class sFCM:
         arr = arr/arr.sum(axis=1)[:, np.newaxis]
 
         return arr
-    
-    def __h(self, u):
-        h = np.zeros((u.shape[0], self.K)) 
 
-        for j in range(u.shape[0]):
-            for i in range(self.K):
-                h[j][i] = sum([u[k][i] for k in self.__nb(j, u.shape[0])])
-        return h 
-
-    def __vi(self, u, im):
+    def _vi(self, u, im):
         '''
         Calculate cluster centers
         '''
@@ -74,68 +56,137 @@ class sFCM:
 
         return vis
     
-    def get_u(self, im, vis, u):
-        up = np.zeros_like(u)
+    def get_u(self, im, vis):
+        up = np.zeros_like(self.u)
 
         for j in range(up.shape[0]):
             for i in range(self.K):
                 up[j][i] = 1.0/sum([np.linalg.norm(im[self.dec(j)] - vis[i])/np.linalg.norm(im[self.dec(j)] - vis[k]) for k in range(self.K)])**(2.0/(self.m-1))
         return up
 
-    def __update_u(self, u, h, vis):
+            
+    def get_class(self, i, j):
+        return np.argmax(self.u[self.enc(i,j),:])
+
+    def step(self, im, eps):
         '''
-        update membership function
+        Runs a single clustering step
+
+        returns a tuple (statistics, converged)
         '''
 
-        up = np.zeros_like(u)
+        #TODO implement convergence criteria based on eps
 
-        for j in range(up.shape[0]):
-            for i in range(up.shape[1]):
-                up[j, i] = (u[j,i]**self.p * h[j, i]**self.q)/sum([u[j, k]**self.p * h[j, k]**self.q for k in range(self.K)])
+        vis = self._vi(self.u, im)
+        self.u = self.get_u(im, vis)
+
+        stats = np.zeros(im.shape)
+        for i in range(stats.shape[0]):
+            for j in range(stats.shape[1]):
+                stats[i,j,:] = vis[fcm.get_class(i,j)]
+
+        return stats, False
         
-        
-        return up
-    def sfcm(self, im, eps):
+    def run(self, im, eps):
         '''
         Runs the iterative process of clustering
 
         im  - np array (h, w, c)
         eps - convergence criteria
+
+        Yields statistics for each iteration
         '''
 
-        self.dec = self.decoder(im.shape[1])
-        self.enc = self.encoder(im.shape[1])
-        
-        #Initialize membership and spatial function
-        u = self.__u_init(*im.shape[:2])
-        #h = self.__h(u)
+        for c in range(FCM.MAX_ITER):
+            stats, finish = self.step(im, eps)
+
+            yield stats, finish
         
 
-        c = 0
-        while c < sFCM.MAX_ITER:
-            vis = self.__vi(u, im)
-            #u   = self.__update_u(u, h, vis)
-            u = self.get_u(im, vis, u)
-            #h = self.__h(u)
 
-            self.u = u
+    
+class sFCM(FCM):
+    MAX_ITER = 50
+    def __init__(self, m, K, p, q, w_siz, imshape):
+        '''
+        Initializes a sFCM algorithm with parameters
 
-            c +=1
+            m       - integer that controls the fuzziness
+            K       - number of classes
+            p       - integer that controls importance of membership function
+            q       - integer that controls importance of spatial function
+            w_size  - odd integer controlling the window size for spatial function (w_size x w_size)
+            imshape - np array (h, w, c)
+            Note (p,q) = (1,0) => regular FCM
+        '''
+        assert w_siz % 2 != 0
 
-            img = np.zeros(im.shape)
+        super(sFCM, self).__init__(m, K, imshape)
 
-            for i in range(img.shape[0]):
-                for j in range(img.shape[1]):
-                    img[i,j,:] = vis[fcm.get_class(i,j)]
+        self.p = p
+        self.q = q
+        self.w_siz = w_siz
 
-            img = cv2.resize(img, (1000, 200)).astype(np.uint8)
-            cv2.imwrite('/home/kristmundur/Documents/KTH/Project Course/sFCM/gifs/{0}.jpeg'.format(c), img)
-            #cv2.imshow("test {}".format(c), img)
-            #cv2.waitKey()
+        #intialize spatial function
+        self.h = self.__h(self.u)
 
         
-    def get_class(self, i, j):
-        return np.argmax(self.u[self.enc(i,j),:])
+
+
+    def __nb(self, j, mx):
+
+        i, j = self.dec(j)
+
+        imx, jmx = self.dec(mx -1)
+        ip, jp = max(0, i - self.w_siz//2), max(0, j - self.w_siz//2)
+
+        for x in range(ip, ip + self.w_siz):
+            for xx in range(jp, jp + self.w_siz):
+                yield self.enc(min(imx, x), min(jmx, xx)) 
+    
+    def __h(self, u):
+        h = np.zeros((u.shape[0], self.K)) 
+
+        for j in range(u.shape[0]):
+            for i in range(self.K):
+                h[j][i] = sum([u[k][i] for k in self.__nb(j, u.shape[0])])
+        return h 
+    
+    def __update_u(self, vis):
+        '''
+        update membership function
+        '''
+
+        up = np.zeros_like(self.u)
+
+        for j in range(up.shape[0]):
+            for i in range(up.shape[1]):
+                up[j, i] = (self.u[j,i]**self.p * self.h[j, i]**self.q)/sum([self.u[j, k]**self.p * self.h[j, k]**self.q for k in range(self.K)])
+        
+        
+        return up
+
+    #@overrides(FCM)
+    def step(self, im, eps):
+        '''
+        Runs a single clustering step
+
+        returns a tuple (statistics, converged)
+        '''
+
+        #TODO implement convergence criteria based on eps
+
+        vis = self._vi(self.u, im)
+        self.u = self.get_u(im, vis)
+        self.h = self.__h(self.u)
+        self.u = self.__update_u(vis)
+
+        stats = np.zeros(im.shape)
+        for i in range(stats.shape[0]):
+            for j in range(stats.shape[1]):
+                stats[i,j,:] = vis[fcm.get_class(i,j)]
+
+        return stats, False
 
 
 import cv2
@@ -143,10 +194,15 @@ import cv2
 test = cv2.imread('test2.png')
 test = cv2.resize(test, (100, 20))
 
-fcm = sFCM(2, 10, 1, 0, 5)
+#fcm = sFCM(2, 10, 1, 0.5, 5, test.shape)
+fcm = FCM(2, 10, test.shape)
 
-print(test.shape)
-fcm.sfcm(test, None)
+for c, (stat, finish) in enumerate(fcm.run(test, 0)):
+    img = cv2.resize(stat, (1000, 200)).astype(np.uint8)
+    cv2.imshow("test {}".format(c), img)
+    cv2.waitKey()
+#print(test.shape)
+#fcm.sfcm(test, None)
 
 
 
