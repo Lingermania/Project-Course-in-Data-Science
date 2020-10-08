de40a5742b282d6e9f003237c41160785d26a601
kristmundur <kristmunduragust@gmail.com>
2020-10-08 19:08:42 +0000

    brainweb and csFCM added

---
+++ brainweb/brainweb_loader.py
@@ -0,0 +1,87 @@
+import brainweb
+from brainweb import volshow
+import numpy as np
+from os import path
+from tqdm.auto import tqdm
+import logging
+import random
+import cv2
+from imutils import build_montages
+
+class brainweb_images:
+    def __init__(self):
+        self.files = None
+
+    def fetch(self):
+        self.files = brainweb.get_files()
+
+    
+    def sample_mMR_dataset(self, parameters, nr_samples, seed = None):
+        '''
+            Samples samples, given a parameter setting
+            parameters : a map with values petNoise, t1Noise, t2Noise, petSigma, t1Sigma, t2Sigma, PetClass
+            nr_samples : number of samples
+            seed       : seed for random number gen
+
+            returns four collections, each of length nr_samples: pet, uMap, T1, T2
+        '''
+        if self.files == None: self.fetch()
+        brainweb.seed(seed)
+        np.random.seed(seed)
+
+        #Compute images
+        pet, uMap, T1, T2 = [],[],[],[]
+
+        #Sampling procedure
+        for f in tqdm(random.sample(self.files, nr_samples), desc='mMR ground truths', unit='subject'):
+            vol = brainweb.get_mmr_fromfile(f, **parameters)
+            pet.append(vol['PET'])
+            uMap.append(vol['uMap'])
+            T1.append(vol['T1'])
+            T2.append(vol['T2'])
+                
+        return pet, uMap, T1, T2
+
+    def _color_mmr_slice(self, mmr_slice, cmap = None):
+        if cmap == None: return mmr_slice
+
+        return cv2.applyColorMap(np.uint8(mmr_slice), cmap)
+
+    def show_mmr_slice(self, mmr_slice, cmap = cv2.COLORMAP_HOT):
+        assert len(mmr_slice.shape) == 2
+        
+        cv2.imshow('t', self._color_mmr_slice(mmr_slice, cmap))
+        
+
+    def show_sample_as_montage(self, mmr_volume, montage_size = 1000, cmap = cv2.COLORMAP_HOT):
+        assert len(mmr_volume.shape) == 3
+
+        nr_images, height, width = mmr_volume.shape
+        montage = [self._color_mmr_slice(mmr_volume[i, :, :], cmap) for i in range(nr_images)]
+        
+        order = int(nr_images**0.5)
+        side  = montage_size//order
+
+
+        montages = build_montages(montage, (side, side), (order, order))
+        for montage in montages:
+
+            cv2.imshow('t', montage)
+            cv2.waitKey()
+        
+
+if __name__ == "__main__":
+    
+    s = brainweb_images()
+    s.fetch()
+    parameters = {'petNoise' : 1, 
+                  't1Noise' : 0.75, 
+                  't2Noise' : 0.75,
+                  'petSigma' : 1, 
+                  't1Sigma' : 1, 
+                  't2Sigma' : 1,
+                  'PetClass' : brainweb.FDG}
+
+    pet, uMap, T1, T2 = s.sample_mMR_dataset(parameters, 1, 1337)
+
+    s.show_sample_as_montage(T1[0])
---
+++ sFCM/sFCM.py
@@ -0,0 +1,260 @@
+import numpy as np
+
+class FCM:
+    MAX_ITER = 50
+    def __init__(self, m, K, imshape):
+        '''
+        Initializes a sFCM algorithm with parameters
+
+            m       - integer that controls the fuzziness
+            K       - number of classes
+            imshape - np array (h, w, c)
+        '''
+        self.m       = m
+        self.K       = K
+        self.imshape = imshape
+
+        #initialize random memberships
+        self.u = self._u_init(*imshape[:2])
+
+        #intialize mapping functions
+        self.dec = self.decoder(imshape[1])
+        self.enc = self.encoder(imshape[1])
+
+
+    def decoder(self, w):
+        return lambda j : (j // w, j % w)
+
+    def encoder(self, w):
+        return lambda i, j : w*i + j
+
+    def _u_init(self, w, h):
+        '''
+        initialize membership function randomly
+        '''
+
+        arr = np.random.random((w*h, self.K))
+
+        #normalize
+        arr = arr/arr.sum(axis=1)[:, np.newaxis]
+
+        return arr
+
+    def _vi(self, u, im):
+        '''
+        Calculate cluster centers
+        '''
+
+        vis = []
+
+        for i in range(self.K):
+            num = sum([u[j][i]**self.m * im[self.dec(j)] for j in range(np.prod(im.shape[:2]))])
+
+            denom = sum([u[j][i]**self.m for j in range(np.prod(im.shape[:2]))])
+
+            vis.append(num/denom)
+
+        return vis
+    
+    def get_u(self, im, vis):
+        up = np.zeros_like(self.u)
+
+        for j in range(up.shape[0]):
+            for i in range(self.K):
+                up[j][i] = 1.0/sum([np.linalg.norm(im[self.dec(j)] - vis[i])/np.linalg.norm(im[self.dec(j)] - vis[k]) for k in range(self.K)])**(2.0/(self.m-1))
+        return up
+
+            
+    def get_class(self, i, j):
+        return np.argmax(self.u[self.enc(i,j),:])
+
+    def step(self, im, eps):
+        '''
+        Runs a single clustering step
+
+        returns a tuple (statistics, converged)
+        '''
+
+        #TODO implement convergence criteria based on eps
+
+        vis = self._vi(self.u, im)
+        self.u = self.get_u(im, vis)
+
+        stats = np.zeros(im.shape)
+        for i in range(stats.shape[0]):
+            for j in range(stats.shape[1]):
+                stats[i,j,:] = vis[fcm.get_class(i,j)]
+
+        return stats, False
+        
+    def run(self, im, eps):
+        '''
+        Runs the iterative process of clustering
+
+        im  - np array (h, w, c)
+        eps - convergence criteria
+
+        Yields statistics for each iteration
+        '''
+
+        for c in range(FCM.MAX_ITER):
+            stats, finish = self.step(im, eps)
+
+            yield stats, finish
+        
+
+
+    
+class sFCM(FCM):
+    MAX_ITER = 50
+    def __init__(self, m, K, p, q, w_siz, imshape):
+        '''
+        Initializes a sFCM algorithm with parameters
+
+            m       - integer that controls the fuzziness
+            K       - number of classes
+            p       - integer that controls importance of membership function
+            q       - integer that controls importance of spatial function
+            w_size  - odd integer controlling the window size for spatial function (w_size x w_size)
+            imshape - np array (h, w, c)
+            Note (p,q) = (1,0) => regular FCM
+        '''
+        assert w_siz % 2 != 0
+
+        super(sFCM, self).__init__(m, K, imshape)
+
+        self.p = p
+        self.q = q
+        self.w_siz = w_siz
+
+        #intialize spatial function
+        self.h = self._h(self.u)
+
+        
+
+
+    def _nb(self, j, mx):
+
+        i, j = self.dec(j)
+
+        imx, jmx = self.dec(mx -1)
+        ip, jp = max(0, i - self.w_siz//2), max(0, j - self.w_siz//2)
+
+        for x in range(ip, ip + self.w_siz):
+            for xx in range(jp, jp + self.w_siz):
+                yield self.enc(min(imx, x), min(jmx, xx)) 
+    
+    def _h(self, u):
+        h = np.zeros((u.shape[0], self.K)) 
+
+        for j in range(u.shape[0]):
+            for i in range(self.K):
+                h[j][i] = sum([u[k][i] for k in self._nb(j, u.shape[0])])
+        return h 
+    
+    def _update_u(self, vis):
+        '''
+        update membership function
+        '''
+
+        up = np.zeros_like(self.u)
+
+        for j in range(up.shape[0]):
+            for i in range(up.shape[1]):
+                up[j, i] = (self.u[j,i]**self.p * self.h[j, i]**self.q)/sum([self.u[j, k]**self.p * self.h[j, k]**self.q for k in range(self.K)])
+        
+        
+        return up
+
+    #@overrides(FCM)
+    def step(self, im, eps):
+        '''
+        Runs a single clustering step
+
+        returns a tuple (statistics, converged)
+        '''
+
+        #TODO implement convergence criteria based on eps
+
+        vis = self._vi(self.u, im)
+        self.u = self.get_u(im, vis)
+        self.h = self._h(self.u)
+        self.u = self._update_u(vis)
+
+        stats = np.zeros(im.shape)
+        for i in range(stats.shape[0]):
+            for j in range(stats.shape[1]):
+                if len(self.imshape) == 3:
+                    stats[i,j,:] = vis[fcm.get_class(i,j)]
+                elif len(self.imshape) == 2:
+                    stats[i,j] = vis[fcm.get_class(i,j)]
+
+        return stats, False
+
+class csFCM(sFCM):
+    MAX_ITER = 30
+    def __init__(self, m, K, p, q, w_siz, imshape):
+        '''
+        Initializes a sFCM algorithm with parameters
+
+            m       - integer that controls the fuzziness
+            K       - number of classes
+            p       - integer that controls importance of membership function
+            q       - integer that controls importance of spatial function
+            w_size  - odd integer controlling the window size for spatial function (w_size x w_size)
+            imshape - np array (h, w, c)
+            Note (p,q) = (1,0) => regular FCM
+        '''
+        assert w_siz % 2 != 0
+
+        super(csFCM, self).__init__(m, K, p, q, w_siz, imshape)
+
+    def get_f(self, im, vis):
+        return self._h(self.u)/self.m
+
+    def step(self, im, eps):
+        '''
+        Runs a single clustering step
+
+        returns a tuple (statistics, converged)
+        '''
+
+        #TODO implement convergence criteria based on eps
+
+        vis = self._vi(self.u, im)
+        self.h = self.get_u(im, vis)
+        self.cu = self.get_f(im, vis) * self.h
+        self.u = self._update_u(vis)
+
+        stats = np.zeros(im.shape)
+        for i in range(stats.shape[0]):
+            for j in range(stats.shape[1]):
+                if len(self.imshape) == 3:
+                    stats[i,j,:] = vis[fcm.get_class(i,j)]
+                elif len(self.imshape) == 2:
+                    stats[i,j] = vis[fcm.get_class(i,j)]
+
+        return stats, False
+
+import cv2
+
+test = cv2.resize(cv2.imread('74.jpeg'), (100,100))
+#test = cv2.cvtColor(cv2.resize(test, (50, 50)), cv2.COLOR_BGR2GRAY)
+
+fcm = sFCM(2, 5, 1, 0.5, 3, test.shape)
+#fcm = FCM(2, 10, test.shape)
+#fcm = csFCM(3, 2, 1, 0.5, 3, test.shape)
+
+for c, (stat, finish) in enumerate(fcm.run(test, 0)):
+    img = cv2.resize(stat, (334, 334)).astype(np.uint8)
+    cv2.imwrite('gifs/{}.jpeg'.format(c), img)
+    #cv2.imshow("test {}".format(c), img)
+    #cv2.waitKey()
+#print(test.shape)
+#fcm.sfcm(test, None)
+
+
+
+
+
+
